d911b04b528a7e1562edb7a72d01015a1692d406
mariamramy25 <mariamramy855@gmail.com>
2025-07-10 22:46:20 +0300

    Add files via upload

---
+++ task 1 CA/script.js
@@ -0,0 +1,14 @@
+Fancybox.bind('[data-fancybox="gallery"]', {
+  Toolbar: {
+    display: [
+      "slideshow",
+      "thumbs",
+      "zoom",
+      "fullscreen",
+      "share",
+      "close",
+    ],
+  },
+  loop: true,
+  protect: true,
+}); 
